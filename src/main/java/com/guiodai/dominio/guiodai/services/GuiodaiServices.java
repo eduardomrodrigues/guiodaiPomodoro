d071c32b06f4ffd5ac3382ca31957a718790d5ac
Eduardo de Moura Rodrigues <eduardom.rodrigues@gmail.com>
2016-04-22 20:28:42 -0300

    #11

--- src/main/java/com/guiodai/dominio/guiodai/services/GuiodaiServices.java
+++ src/main/java/com/guiodai/dominio/guiodai/services/GuiodaiServices.java
@@ -1,5 +1,8 @@
 package com.guiodai.dominio.guiodai.services;
 
+import java.io.IOException;
+
+import javax.net.ssl.SSLContext;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
@@ -7,16 +10,44 @@ import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.Form;
 import javax.ws.rs.core.MediaType;
 
+import org.apache.commons.io.IOUtils;
+import org.glassfish.jersey.SslConfigurator;
+
 public class GuiodaiServices {
+
+	private SSLContext sslContext;
+
+	public GuiodaiServices() {
+
+		System.setProperty("jsse.enableSNIExtension", "false");
+		
+		byte[] bTrustStore = null;
+		byte[] bKeyFile = null;
+		try {
+		
+			bTrustStore = IOUtils.toByteArray(GuiodaiServices.class.getResourceAsStream("/ssl/cacerts.jks"));
+			bKeyFile = IOUtils.toByteArray(GuiodaiServices.class.getResourceAsStream("/ssl/keystore.jks"));
+		
+		
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		
+		SslConfigurator sslConfigurator = SslConfigurator.newInstance().trustStoreBytes(bTrustStore)
+				.trustStorePassword("changeit").keyStoreBytes(bKeyFile).keyPassword("changeit");
+		sslContext = sslConfigurator.createSSLContext();
+
+	}
 
 	public PomodoroIssue recuperarPomodoro(Long userId, Long issueId) throws PomodoroNotFoundException {
 
 		PomodoroIssue p = new PomodoroIssue();
 		try {
 
-			Client client = ClientBuilder.newClient();
+			Client client = ClientBuilder.newBuilder().sslContext(sslContext).build();
 			WebTarget target = client.target(
-					"http://www.guiodai.com/pomodoro/services/pomodoro/userid/" + userId + "/issueid/" + issueId);
+					"https://www.guiodai.com/pomodoro/services/pomodoro/userid/" + userId + "/issueid/" + issueId);
 
 			p = target.request(MediaType.APPLICATION_JSON_TYPE).get(PomodoroIssue.class);
 		} catch (javax.ws.rs.NotFoundException e) {
@@ -28,9 +59,9 @@ public class GuiodaiServices {
 
 	public void incrementarPomodoro(Long userId, Long issueId) {
 
-		Client client = ClientBuilder.newClient();
+		Client client = ClientBuilder.newBuilder().sslContext(sslContext).build();
 
-		WebTarget target = client.target("http://www.guiodai.com/pomodoro/services/pomodoro");
+		WebTarget target = client.target("https://www.guiodai.com/pomodoro/services/pomodoro");
 
 		Form form = new Form();
 		form.param("userId", "" + userId);
