380470288008023d404f01cbdb7d70851b46de1b
Eduardo de Moura Rodrigues <eduardom.rodrigues@gmail.com>
2017-04-04 22:24:47 -0300

    Consertar o pomodoro git hub #3

--- src/main/java/com/guiodai/dominio/DDLCreator.java
+++ src/main/java/com/guiodai/dominio/DDLCreator.java
@@ -1,5 +1,23 @@
 package com.guiodai.dominio;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class DDLCreator {
 
+	public static void fiatLux(){
+		
+		
+		Connection con = H2Connection.getConnection();
+		try {
+			Statement st = con.createStatement();
+			st.executeQuery("CREATE TABLE USER (ID VARCHAR(255), ETAPA INT)");
+		
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
 }
